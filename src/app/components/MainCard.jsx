d6cf08499cb220b003aa0627e56adce354704036
MaMasoud2 <hpdhooda@gmail.com>
2024-04-02 06:33:15 +0200

    resposive

--- src/app/components/MainCard.jsx
+++ src/app/components/MainCard.jsx
@@ -2,7 +2,7 @@ import Main from "./Main"
 
 export default function MainCard() {
     return (
-        <Main className='sm:flex-col md:flex-col lg:hover:translate-x-4'>
+        <Main className='sm:flex-col md:flex-col lg:hover:translate-x-4 sm:-translate-y-4'>
             <img src="photo.png" alt=""/>
             <div className="info flex flex-col gap-5 sm:items-center md:items-center ">
                 <div className="name text-5xl sm:text-2xl text-center">I’m Abdelrahman Nasr</div>
