60926497cf59a42dbf643c5175437248b0cd81da
MaMasoud2 <hpdhooda@gmail.com>
2024-04-02 06:38:24 +0200

    resposive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <MyLayout />
       <MainCard />
-      <div className="absolute bottom-6 sm:bottom-20 p-5 left-0 flex justify-center items-center w-full"><Link href="/skills" className="text-sm hover:scale-125 duration-300">See My Skills</Link></div>
+      <div className="fixed bottom-6 p-5 left-0 flex justify-center items-center w-full sm:top-6 sm:left-6 sm:bottom-auto sm:justify-start z-40"><Link href="/skills" className="text-sm hover:scale-125 duration-300">See My Skills</Link></div>
     </>
   );
 }
